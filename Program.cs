ccabd24e5004f6d732ee425c2de34b5f9b8c8d34
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-03 22:04:03 +0000

    C# methods

--- Program.cs
+++ Program.cs
@@ -4,15 +4,34 @@ namespace MyFirstProgram
 {
     class Program
     {
-        // Arrays in C#
+        // C# methods
         static void Main(string[] args)
         {
-            String[] car = { "BMW", "Mustang", "Corvette" };
-            foreach (String s in car)
-            {
-                Console.WriteLine(s);
-            }
+            String name = "Sulemana Abubakar Saddique";
+            int age = 20;
+            String residence = "Ashaley Botwe";
             
+            myBirthInfo(name,age,residence);
+            Console.WriteLine();
+            singHappyBirthday(name, age);
+            
+
+        }
+
+        static void myBirthInfo(String name, int age, String residence)
+        {
+            Console.WriteLine("My name is "+name);
+            Console.WriteLine("I am "+age+ " years old");
+            Console.WriteLine("I stay at "+residence);
+            
+        }
+
+        static void singHappyBirthday(String name, int age)
+        {
+            Console.WriteLine("Happy Birthday to you");
+            Console.WriteLine("Happy Birthday to you");
+            Console.WriteLine("Happy Birthday dear "+name);
+            Console.WriteLine("You are "+age+" years old now");
         }
     }
 }
