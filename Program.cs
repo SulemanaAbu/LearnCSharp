d944a4516eec0dd4d4eb1099df3602d8d6fd298f
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-03 16:59:17 +0000

    nested loop

--- Program.cs
+++ Program.cs
@@ -4,19 +4,27 @@ namespace MyFirstProgram
 {
     class Program
     {
-        //while loop
+        // nested for loop
         static void Main(string[] args)
         {
-            String name = "";
-           
+            Console.WriteLine("How many rows? ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("How many columns? ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("What symbol?  ? ");
+            String symbol = Console.ReadLine();
 
-            while (name == "")
+            for (int i = 0; i < rows ; i++)
             {
-                Console.WriteLine("Enter your name: ");
-                name = Console.ReadLine();
-            }
+                for (int j = 0; j < columns ; j++)
+                {
+                    Console.Write(symbol);
+                }
 
-            Console.WriteLine("Hello "+name);
+                Console.WriteLine();
+            }
         }
     }
 }
