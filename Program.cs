7ae1d8e0679b8cc49d7a9809344784ea2e8c9152
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-03 13:34:33 +0000

    Variables

--- Program.cs
+++ Program.cs
@@ -1,19 +1,35 @@
 ﻿using System;
 
-
 namespace MyFirstProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //This is a comment
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Let's get it");
+            int x = 123; // declaration
+           // initialization
+
+            int y = 321; // declaration + initialization
+
+            int z = x + y;
+
+            int age = 21; // whole integer
+            double height = 300.5; // decimal number
+            bool alive = false; //true or false
+            char symbol = '@'; // single character
+            String name = "Bro"; // a series of characters
             
-            //Escape sequence 
-            Console.WriteLine("\tBroCode");
-            Console.Beep();
+            Console.WriteLine(z);
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Your age is " + age);
+            Console.WriteLine("Your height is " + height + "cm");
+            Console.WriteLine("Are you alive? " + alive);
+            Console.WriteLine("Your symbol is: " + symbol);
+
+            String userName = symbol + name;
+
+            Console.WriteLine("Your username is: " + userName);
+
             Console.ReadKey();
         }
     }
