f06331e0e0f1622688878c225444b3de63805135
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-03 21:25:51 +0000

    arrays for loop

--- Program.cs
+++ Program.cs
@@ -4,62 +4,15 @@ namespace MyFirstProgram
 {
     class Program
     {
-        // Calculator Program 
+        // Arrays in C#
         static void Main(string[] args)
         {
-            do
+            String[] car = { "BMW", "Mustang", "Corvette" };
+            for (int i =0; i < car.Length; i++)
             {
-                double num1 = 0;
-                double num2 = 0;
-                double result = 0;
-           
-
-                Console.WriteLine("--------------------");
-                Console.WriteLine("Calculator Program");
-                Console.WriteLine("--------------------");
-
-                Console.WriteLine("Enter first number: ");
-                num1 = Convert.ToDouble(Console.ReadLine());
-
-                Console.WriteLine("Enter second number: ");
-                num2 = Convert.ToDouble(Console.ReadLine());
-
-                Console.WriteLine("Select an option");
-                Console.WriteLine("\t+ : Add");
-                Console.WriteLine("\t- : Subtract");
-                Console.WriteLine("\t* : Multiply");
-                Console.WriteLine("\t/ : Divide");
-
-                Console.WriteLine("Enter an option");
-
-                switch (Console.ReadLine())
-                {
-                    case "+":
-                        result = num1 + num2;
-                        Console.WriteLine($"The result: {num1} + {num2} = " + result);
-                        break;
-                    case "-":
-                        result = num1 - num2;
-                        Console.WriteLine($"The result: {num1} - {num2} = " + result);
-                        break;
-                    case "*":
-                        result = num1 * num2;
-                        Console.WriteLine($"The result: {num1} * {num2} = " + result);
-                        break;
-                    case "/":
-                        result = num1 / num2;
-                        Console.WriteLine($"The result: {num1} / {num2} = " + result);
-                        break;
-                    default:
-                        Console.WriteLine("That was not a valid option");
-                        break;
-                }
-
-                Console.WriteLine("Would you like to continue (Y = yes)/ (N = no)");
-            } while (Console.ReadLine().ToUpper() == "Y")
-                ;{
-                Console.WriteLine("Byeee!");
+                Console.WriteLine(car[i]);
             }
+            
         }
     }
 }
