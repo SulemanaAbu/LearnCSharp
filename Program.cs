1057963d3aeeac5db3659d80b398ae2c11efcaa5
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-03 12:04:53 +0000

    escape sequence

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Let's get it");
-Console.Beep();
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a comment
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Let's get it");
+            
+            //Escape sequence 
+            Console.WriteLine("\tBroCode");
+            Console.Beep();
+            Console.ReadKey();
+        }
+    }
+}
