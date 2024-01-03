19d004fcfda23f35f117b68e367ab14849690025
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-03 14:01:49 +0000

    accept user input

--- Program.cs
+++ Program.cs
@@ -6,10 +6,16 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-           //Accept user input
-           Console.WriteLine("What is your name? ");
-           String name = Console.ReadLine();
-           Console.WriteLine("Hello "+name);
+            //Accept user input
+            Console.WriteLine("What is your name? ");
+            String name = (Console.ReadLine());
+            Console.WriteLine("Hello " + name);
+
+            Console.WriteLine("What is your age? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age " + age);
+            
+
         }
     }
-}
+}    
