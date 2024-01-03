a14dbfecfb4c895a1148f5eb5faf8b0044f93ce9
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-03 14:44:58 +0000

    almighty formula

--- Program.cs
+++ Program.cs
@@ -4,22 +4,35 @@ namespace MyFirstProgram
 {
     class Program
     {
-        //Finding the hyputenuse of a triange
+        //The almighty formula of quadratic expressions
         static void Main(string[] args)
         {
-            Console.WriteLine("A program to find the hypotenuse of a triangle");
+            Console.WriteLine("The almighty formula for quadratic expressions");
+
+            Console.WriteLine("Enter the coefficient of x-squared (a): ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The coefficient of x-squared is "+a);
             
+            Console.WriteLine("Enter the coefficient of x (b): ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The coefficient of x is "+b);
             
-            Console.WriteLine("Enter side A: ");
-            double a =Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Side A is "+a);
+            Console.WriteLine("Enter the constant (c): ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The constant is "+c);
+
+            double pos = -b + Math.Sqrt((b * b) - (4 * a * c));
+            double neg = -b - Math.Sqrt((b * b) - (4 * a * c));
+
+            double X1 = pos / (2 * a);
+            double X2 = neg / (2 * a);
             
-            Console.WriteLine("Enter side B: ");
-            double b =Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Side B is "+b);
-            
-            double c = Math.Sqrt((a * a) + (b * b));
-            Console.WriteLine("The hypotenuse of the triangle is "+c);
+            Console.WriteLine("The first result of the quadratic is "+X1);
+            Console.WriteLine("The second result of the quadratic is "+X2);
+
+
+
+
         }
     }
 }
