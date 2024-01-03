db831fc8c6cfe655c19a676aadedae88664e1afc
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-03 14:12:05 +0000

    math class

--- Program.cs
+++ Program.cs
@@ -6,16 +6,22 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-            //Accept user input
-            Console.WriteLine("What is your name? ");
-            String name = (Console.ReadLine());
-            Console.WriteLine("Hello " + name);
 
-            Console.WriteLine("What is your age? ");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your age " + age);
+            double x = 3;
+            double y = 5;
+
+            double a = Math.Pow(x, 2);
+            double b = Math.Sqrt(x);
+            double c = Math.Abs(x);
+            double d = Math.Round(x);
+            double e = Math.Ceiling(x);
+            double f = Math.Floor(x);
+            double g = Math.Max(x, y);
+            double h = Math.Min(x, y);
+
+            Console.WriteLine(a);
+
             
-
         }
     }
-}    
+}
