83874938d00c0b369209faef7d8957ec0ca09734
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-04 14:10:05 +0000

    classes

---
+++ Messages.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.Design;
+
+namespace MyFirstProgram;
+
+ static class Messages
+{
+    
+       public  static void Hello()
+        {
+            Console.WriteLine("Hello, welcome");
+        }
+        public static void Wait()
+        {
+            Console.WriteLine("I am waiting for someone"); 
+        }
+
+        public static void Bye()
+        {
+            Console.WriteLine("Byeee!");
+        }
+    
+}
--- Program.cs
+++ Program.cs
@@ -4,42 +4,13 @@ namespace MyFirstProgram
 {
     class Program
     {
-        // exception handling
+        // 
         static void Main(string[] args)
         {
-            try
-            {
-                double x;
-                double y;
-                double z;
-
-                Console.WriteLine("Enter the value of x: ");
-                x = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Enter the value of y: ");
-                y = Convert.ToInt32(Console.ReadLine());
-
-                z = x / y;
-                Console.WriteLine($"The result of {x} / {y} = " + z);
-            }
-            catch (FormatException e)
-            {
-                Console.WriteLine("Enter only numbers please!");
-            }
-            catch (DivideByZeroException e)
-            {
-                Console.WriteLine("You can't Divide by zero Dummy!!");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Something went wrong!!!");
-            }
-            finally
-            {
-                Console.WriteLine("Thank you!");
-            }
+         Messages.Hello();
+         Messages.Wait();
+         Messages.Bye();
         }
         
-
     }
 }
