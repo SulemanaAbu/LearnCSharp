2e7952f372d525ca747b131465a57355419febeb
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-04 14:22:31 +0000

    Objects

---
+++ Human.cs
@@ -0,0 +1,17 @@
+namespace MyFirstProgram;
+
+public class Human
+{
+    public String name;
+    public int age;
+
+    public  void speak()
+    {
+        Console.WriteLine(name + " is speaking");
+    }
+
+    public void eat()
+    {
+        Console.WriteLine(name+" is eating");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,12 +4,17 @@ namespace MyFirstProgram
 {
     class Program
     {
-        // 
+        // Objects
         static void Main(string[] args)
         {
-         Messages.Hello();
-         Messages.Wait();
-         Messages.Bye();
+            Human human1 = new Human();
+            Human human2 = new Human();
+
+            human1.name = "Rick";
+            human2.name = "Saddiq";
+            
+            human1.speak();
+            human2.eat();
         }
         
     }
