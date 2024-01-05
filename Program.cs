b9f4ec8feebaca280b33570b21e28f14c60dde30
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-05 17:41:43 +0000

    Enums

--- Program.cs
+++ Program.cs
@@ -9,6 +9,19 @@ namespace MyFirstProgram
         {
             Console.WriteLine(Planet.Pluto+" is a planet");
             Console.WriteLine(Planet.Mercury+" is  planet #"+(int)Planet.Mercury);
+
+            String name = planetRadius.Earth.ToString();
+            int radius = (int)planetRadius.Earth;
+            Console.WriteLine("planet: "+name);
+            Console.WriteLine("radius: "+radius+" km");
+            double volume = Volume(planetRadius.Earth);
+            Console.WriteLine("The volume of the earth is: "+volume+" km^3");
+        }
+
+        public static double Volume(planetRadius radius)
+        {
+            double volume = (4.0 / 3.0) * Math.PI * Math.Pow((int)radius, 3);
+            return volume;
         }
 
     }
---
+++ planetRadius.cs
@@ -0,0 +1,14 @@
+namespace MyFirstProgram;
+
+public enum planetRadius
+{
+    Mercury =2439,
+    Venus = 6051,
+    Earth = 6371,
+    Mars = 3389,
+    Jupiter = 69911,
+    Saturn = 58232,
+    Uranus = 25362,
+    Neptune = 24622,
+    Pluto = 1188
+}
