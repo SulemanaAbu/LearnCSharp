afd014cf8cfde52df8e221e2d9f2de732140b8b3
SulemanaAbu <assulemana003@st.ug.edu.gh>
2024-01-05 07:28:16 +0000

    Arguments

--- Car.cs
+++ Car.cs
@@ -3,9 +3,11 @@ namespace MyFirstProgram;
 public class Car
 {
   public String model;
+  public String color;
 
-  public Car(String model)
+  public Car(String model, String color)
   {
     this.model = model;
+    this.color = color;
   }
 }
--- Program.cs
+++ Program.cs
@@ -4,22 +4,38 @@ namespace MyFirstProgram
 {
     class Program
     {
-        // Abstract classes
+        // Arguments
         static void Main(string[] args)
         {
-            Car[] garage = new Car[3];
             
-            Car car1 = new Car("Ferrari");
-            Car car2 = new Car("Tate");
-            Car car3 = new Car("Tesla");
+            Car car1 = new Car("Ferrari","Yellow");
+            Car car2 = new Car("Tate","Blue");
+            Car car3 = new Car("Tesla","Sliver");
 
-            foreach (Car car in garage)
-            {
-                Console.WriteLine(car.model);
-            }
+            Car car4 = makeCopy(car1);
+            
+            changeColor(car1,"Blackish");
+            Console.WriteLine(car1.color);
+            changeModel(car2,"Andrew");
+            Console.WriteLine(car2.model);
+            Console.WriteLine(car4.model +" "+ car1.color);
+            
             
 
         }
-        
+        public static void changeColor(Car car, String color)
+        {
+            car.color = color;
+        }
+
+        public static void changeModel(Car car, String model)
+        {
+            car.model = model;
+        }
+
+        public static Car makeCopy(Car car)
+        {
+            return new Car(car.model, car.color);
+        }
     }
 }
